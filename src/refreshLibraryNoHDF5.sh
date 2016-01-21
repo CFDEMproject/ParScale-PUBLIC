f55b570636aaad5aa0505970864f98c300a3b409
Stefan Radl <radl@tugraz.at>
2016-01-21 16:32:15 +0100

    added refreshLibrary with fedora_fpic_noHDF5

---
+++ src/refreshLibraryNoHDF5.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#***************************************
+# Compiles ParScale LIBRARY
+#***************************************
+
+#copy coupling files from package to LIGGGHTS/src
+cd $PASCAL_LIGGGHTS_SRC_DIR/PASCAL
+chmod 777 Install.sh
+./Install.sh 0
+./Install.sh 1
+
+#clean and remove all copulings to 
+cd $PASCAL_SRC_DIR/COUPLING_LIGGGHTS
+chmod 777 Install.sh
+./Install.sh 0
+./Install.sh 1
+cd $PASCAL_SRC_DIR
+
+make clean-all
+
+
+#Compile
+make thirdParty -j 4
+make -f makelib
+make -f Makefile.lib fedora_fpic_noHDF5 -j 4
